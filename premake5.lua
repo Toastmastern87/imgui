965ff284bbe185bdb6fc2ed862e7e8c37b20bcf2
Aho Johan <johan.aho@scania.com>
2024-07-24 09:03:40 +0200

    Fixing premake file errors

--- premake5.lua
+++ premake5.lua
@@ -1,44 +1,47 @@
-project ImGui
-	kind StaticLib
-	language C++
-    staticruntime off
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
 
-	targetdir (bin .. outputdir .. %{prj.name})
-	objdir (bin-int .. outputdir .. %{prj.name})
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "pch.h"
+	pchsource "Src/pch.cpp"
 
 	files
 	{
-		imconfig.h,
-		imgui.h,
-		imgui.cpp,
-		imgui_draw.cpp,
-		imgui_internal.h,
-		imgui_tables.cpp,
-		imgui_widgets.cpp,
-		imstb_rectpack.h,
-		imstb_textedit.h,
-		imstb_truetype.h,
-		imgui_demo.cpp
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
 	}
 
-	filter systemwindows
-		systemversion latest
-		cppdialect C++17
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
 
-	filter systemlinux
-		pic On
-		systemversion latest
-		cppdialect C++17
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
 
-	filter configurationsDebug
-		runtime Debug
-		symbols on
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
 
-	filter configurationsRelease
-		runtime Release
-		optimize on
-
-    filter configurationsDist
-		runtime Release
-		optimize on
-        symbols off
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
