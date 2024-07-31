e04c2c6761a1c073c234405e5664be27cc3d01c0
Toastmastern <johanx.aho@gmail.com>
2024-07-31 09:46:13 +0200

    Fixing premake file

--- premake5.lua
+++ premake5.lua
@@ -16,7 +16,7 @@ project "ImGui"
 		"imgui_internal.h",
 		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
-		"imstb_rectpack.h,"
+		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
 		"imgui_demo.cpp"
